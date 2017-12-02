655c99812aa0dd3379dca7868d08da955e7a612d
Mimi Chenyao <mimichenyao@gmail.com>
2017-12-02 16:56:26 -0500

    Update LSDStringSort.java

--- LSDStringSort.java
+++ LSDStringSort.java
@@ -1,43 +1,6 @@
 public class LSDStringSort {
 
-	// Sorts strings by least significant digit
-	public void sort(String[] original, int digitIndex) {
-
-		int R = 256; // extended ASCII alphabet size 
-		int N = original.length;
-		String[] sorted = new String[N];
-
-		// Represents each pass through the array 
-		for (int stringIndex = digitIndex-1; stringIndex >= 0; stringIndex--) {
-
-			int[] count = new int[R+1];
-
-			// count array: tallies up count of each letter
-			// and makes running count 
-			for (int i = 0; i < N; i++) {
-				count[original[i].charAt(stringIndex)+1]++;
-			}
-
-			// count array: shift one down
-			for (int r = 0; r < R; r++) {
-				count[r+1] += count[r];
-			}
-
-			// count array: populate sorted array
-			for (int i = 0; i < N; i++) {
-				sorted[count[original[i].charAt(stringIndex)]++] = original[i];
-			}
-
-			// copy over sorted array into original array
-			for (int i = 0; i < N; i++) {
-				original[i] = sorted[i];
-			}
-		}
-	}
-
-	/// variable code
-
-	// Finds longest length string in an array of strings
+    // Finds longest length string in an array of strings
     public static int findLongestLength(String[] array) {
 
         int longest = 0;
